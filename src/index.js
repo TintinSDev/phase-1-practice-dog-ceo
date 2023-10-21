60475fe4b2076862cfb1253d6b7c7632294afd3e
Martin <mmaina290@gmail.com>
2023-10-21 05:58:12 +0300

    Fetch Dog

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+const breedDropdown = document.querySelector('#breed-dropdown')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => {
+        const img = document.createElement('img')
+        img.src = data.message[0]
+        document.body.appendChild(img)
+    })
+
+    fetch(breedUrl)
+        .then(response => response.json())
+        .then(data => {
+            const ul = document.createElement('ul')
+            for (let breed in data.message) {
+                const li = document.createElement('li')
+                li.innerText = breed
+                li.addEventListener('click', onclick => {
+                    li.style.color = 'red'
+                    // fetch(breed).then(res => res.json()).then(data => {
+                    //     const img = document.createElement('img')
+                    //     img.src = data.message[0]
+                    //     document.body.appendChild(img)
+
+                    // })
+                })
+                ul.appendChild(li)
+            }
+            document.body.appendChild(ul)
+        })
+    
+})
